420894c389c6b6a44d19cac636d909e54a77d68f
Kayla <6lackmacaroon@gmail.com>
2019-04-03 13:41:14 -0600

    first two principles for this.js done

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,13 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. 
+Window binding - avoid! `this` isn't pointing at anything specific so it looks to its parent element, the window. which very nearly never does anything good.
 * 2. 
+Implicit binding - is automatic, it is used for well defined objects to use `this` in their methods . Easy to spot.
 * 3. 
+New binding - uses a constructor function (original artwork) to create new objects (copies), uses prototypes (stencils) to pass around frequently used methods (pencils/ink/paint)
 * 4. 
+Explicit binding - uses .apply() to pass in an array as arguments && uses .bind() to save a keyword for use later && uses .call() allows a single method or function to be used by another object.
 *
 * write out a code example of each explanation above
 */
@@ -12,10 +16,26 @@
 // Principle 1
 
 // code example for Window Binding
+function sup(name){
+      return `Sup ${name}?`;
+}
+
+console.log(sup("dude"));
+
 
 // Principle 2
 
 // code example for Implicit Binding
+const me = {
+      name: "Kayla",
+      age: 31,
+      food: "cake",
+      favorite: function() {
+        return `${this.name} loves ${this.food}`;
+      }
+    }
+
+console.log(me.favorite());
 
 // Principle 3
 
